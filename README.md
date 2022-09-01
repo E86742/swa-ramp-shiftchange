d3f7478c97e9fbe4aba7b1626b3e6d6005d27a2f
E86742 <106930021+E86742@users.noreply.github.com>
2022-09-01 06:34:22 -0600

    Rough Draft Roadmap Plan

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SWA Ramp Shift Change
 Web utility to handle shift change via ramp schedule download, convert to CSV, upload automatically for parsing.
+
+
+# Libraries
--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 # Ramp Schedule Optimizer
 
+import pdftables_api
+
+# import ramp schedule in PDF format
+# Convert PDF to CSV file for file parsing
+# Format CSV
+# Read CSV and import data into lists
+# Read and loop through lists
+# Output times
+# Nested Loop in order to display closure times
+# Generate report of closures
+
+
+
+def main():
+    print("main")
+main()
